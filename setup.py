509c65b2ee41de2751ef41fee6c6517443741cc4
Ubuntu <ubuntu@ip-172-31-42-37.eu-central-1.compute.internal>
2020-05-07 13:56:10 +0000

    changed from hyphen to underscore

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.0.1',
     description='Private VNR package with most commonly used functions',
     url='git@github.com:vnrag/data-utils.git',
-    packages=['data-utils'],
+    packages=['data_utils'],
     install_requires=['pandas'] #external packages as dependencies
 )
