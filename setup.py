9631a76c1c6b221ce7b4ba0bd41553d248604f2e
Ubuntu <ubuntu@ip-172-31-42-37.eu-central-1.compute.internal>
2020-05-07 14:35:24 +0000

    iwx.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='data_utils',
     version='0.0.1',
     description='Private VNR package with most commonly used functions',
+    license='VNR Verlag für die Deutsche Wirtschaft AG',
     url='git@github.com:vnrag/data-utils.git',
     packages=['data_utils'],
     install_requires=['pandas'] #external packages as dependencies
