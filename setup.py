72add66f4a0551a79d5aeda2447e383d1392cf4b
Ubuntu <ubuntu@ip-172-31-42-37.eu-central-1.compute.internal>
2020-05-07 13:09:23 +0000

    added sample functions for initial test

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='data_utils',
+    version='0.0.1',
+    description='Private VNR package with most commonly used functions',
+    url='git@github.com:vnrag/data-utils.git',
+    packages=['data-utils'],
+    install_requires=['pandas'] #external packages as dependencies
+)
