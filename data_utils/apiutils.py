c81bada3488504deec283d95d1e8bba412123234
ptyma <chettripratima@gmail.com>
2020-05-26 16:34:57 +0200

    post request added on apiutils

--- data_utils/apiutils.py
+++ data_utils/apiutils.py
@@ -28,3 +28,33 @@ def handle_get_request(url, headers=None):
 		print(r.data)
 		raise SystemExit
 	return json.loads(r.data.decode('utf-8'))
+
+
+def handle_post_request(url, body=None, headers=None):
+	"""Requests data from the provided URL via a get request
+	Parameters
+	----------
+	url : String
+		The URL to post data and get results
+	body: Json
+		Contents for the post request
+	headers: Json
+		Needed arguments for post request
+		Eg: {'Content-Type': 'application/json'}
+	Returns
+	-------
+	json
+		Description
+	Raises
+	------
+	SystemExit
+		Description
+	"""
+
+	http = urllib3.PoolManager()
+	encoded_body = json.dumps(body).encode('utf-8')
+	r = http.request('POST', url, body=encoded_body, headers=headers)
+	if r.status != 200:
+		print(r.data)
+		raise SystemExit
+	return json.loads(r.data.decode('utf-8'))
