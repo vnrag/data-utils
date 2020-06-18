0354bc38324e528373eb7a62638a364200c650b5
isma3ilsamir <45247141+isma3ilsamir@users.noreply.github.com>
2020-06-18 17:24:14 +0200

    changed required python version
    changed required python version to 3.6 for emr bootstrap installation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ NAME = 'data_utils'
 DESCRIPTION = 'Private VNR package with most commonly used functions'
 URL = 'git@github.com:vnrag/data-utils.git'
 VERSION = '0.1.0'
-REQUIRES_PYTHON = '>=3.7.0'
+REQUIRES_PYTHON = '>=3.6.0'
 
 
 # What packages are required for this module to be executed?
