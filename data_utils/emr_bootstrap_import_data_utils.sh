c15a87ca745370231a9721f09e7e45b6c66714f0
isma3ilsamir <45247141+isma3ilsamir@users.noreply.github.com>
2020-09-07 13:59:03 +0200

    Update emr_bootstrap_import_data_utils.sh
    changed it from using " iwx_sequential" branch to the master branch

--- data_utils/emr_bootstrap_import_data_utils.sh
+++ data_utils/emr_bootstrap_import_data_utils.sh
@@ -3,4 +3,4 @@ set -x -e
 sudo yum update -y
 sudo yum install -y git python3-pip
 sudo ln -sf /usr/bin/python3 /usr/bin/python
-sudo pip-3.6 install git+https://git@github.com/vnrag/data-utils.git@iwx_sequential_add
+sudo pip-3.6 install git+https://git@github.com/vnrag/data-utils.git
