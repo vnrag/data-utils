0c6d54c054ffb45c20c6cba576e2d6b81584563c
isma3ilsamir <isma3il.samir@gmail.com>
2021-03-30 16:38:24 +0200

    try to freeze libraries to match ones from requirements file in data-api-requester

--- setup.py
+++ setup.py
@@ -10,15 +10,16 @@ REQUIRES_PYTHON = '>=3.6.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'pandas',
     'urllib3',
-    'boto3',
-    'botocore',
-    'pyarrow',
-    's3fs',
     'docopt',
     'python-dotenv',
     'awswrangler'
+    'pyarrow==1.0.0'
+    's3fs==0.4.2'
+    'moto==1.3.14'
+    'botocore==1.17.44'
+    'boto3==1.14.0'
+    'pandas==1.1.0'
 ]
 
 # Import the README and use it as the long-description.
